46d2eb048cc752e52141cb31f14e8d62fe04834a
C-dub <25037768+ColeS82@users.noreply.github.com>
2022-12-24 10:18:15 -0700

    modified welcome

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,8 +6,11 @@ export default function Home() {
     return(
         <div id='home-main'>
             <Particle></Particle>
-            <div id='hello' className='d-flex card align-items-center container justify-content-center col-4'>Welcome to my portfolio
-                
+            <div id='hello' className='d-flex card align-items-center container justify-content-center col-4'>
+                <h2>Welcome</h2>
+                <p>
+                    To Cole Sexson's Portfolio
+                    </p>
                 <Link to ='/Aboutme'>
                 <button className='btn bg-primary mt-5'>About Me
 

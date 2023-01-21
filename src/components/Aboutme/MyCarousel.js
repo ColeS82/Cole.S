67fb7fc23df382f74870784ea2276a3b3276390d
C-dub <25037768+ColeS82@users.noreply.github.com>
2023-01-20 19:42:33 -0700

    styled crypto link and indented <p> above it.

--- src/components/Aboutme/MyCarousel.js
+++ src/components/Aboutme/MyCarousel.js
@@ -9,7 +9,7 @@ export default function myCarousel() {
       <Carousel className='carousel'>
         <Carousel.Item interval={5000}>
           <Link to='/Crypto'>
-          <img id='cryptoImage'
+          <img
             className="d-block w-100 my-5 rounded"
             src={crypto}
             alt="First slide"
--- src/components/Crypto/crypto.js
+++ src/components/Crypto/crypto.js
@@ -4,7 +4,7 @@ import Ticker from '../Tickers/Ticker'
 export default function Crypto () {
     return(
         <div className='container py-5 mt-3'>
-            <p className='text-white h4 w-75 mx-auto' style={{fontFamily: 'Georgia, serif'}}>
+            <p className='text-white h4 w-75 mx-auto' style={{fontFamily: 'Georgia, serif', textIndent: '50px' }}>
                 Here is a simple crypto ticker that I made using axios.get to fetch the image, id and price. I included buttons linked to the white papers that created these projects for those interested in the technology.
             </p>
             <Ticker />

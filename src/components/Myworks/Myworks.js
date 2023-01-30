766732410b8dd57a06f3db49a58500eab17cf96a
C-dub <25037768+ColeS82@users.noreply.github.com>
2023-01-29 18:57:08 -0700

    added noteTaker to my works page

--- src/components/Myworks/Myworks.js
+++ src/components/Myworks/Myworks.js
@@ -1,6 +1,7 @@
 import myWorks from './myWorks.css'
 import { Link } from 'react-router-dom'
 import crypto from '../../images/crypto.jpeg'
+import noteTaker from '../../images/noteTaker.png'
 import onlyDevs from '../../images/only-devs.png'
 import antidrink from '../../images/antidrink.webp'
 export default function Myworks() {
@@ -15,7 +16,7 @@ export default function Myworks() {
                     <h5 className='border-bottom mx-5 pb-1'>
                         My Crypto Ticker
                     </h5>
-                    <div className="card mt-sm-5 ">
+                    <div className="card mt-sm-5 bg-secondary">
                         <Link to='/crypto'>
                         <div className="card-body">
                             <img className='rounded' src={crypto} style={{maxWidth: '100%'}}>
@@ -29,7 +30,7 @@ export default function Myworks() {
                     <h5 className='border-bottom mx-5 pb-1'>
                         OnlyDevs
                     </h5>
-                    <div className="card mt-sm-3">
+                    <div className="card mt-sm-3 bg-secondary">
                         <div className="card-body">
                             <a href='https://the-only-devs.herokuapp.com/' target='_blank'>
                                 <img className='rounded' src={onlyDevs} style={{maxWidth: '100%'}}>
@@ -43,7 +44,7 @@ export default function Myworks() {
                     <h5 className='border-bottom mx-4 pb-1'>
                         The Anti Social Drinker App
                     </h5>
-                    <div className="card mt-sm-3">
+                    <div className="card mt-sm-3 bg-secondary">
                         <div className="card-body">
                         <a href='https://trufl.github.io/nonsocial-drinker/' target='_blank'>
                             <img className='rounded' src={antidrink} style={{maxHeight: '130px'}}>
@@ -55,38 +56,43 @@ export default function Myworks() {
             </div>
 
             <div className='d-sm-flex container justify-content-around mx-auto text-white text-center mt-sm-5'>
-                <section className="col-sm-3 works">
-                    <h5>
-                        Card 4
+                <section className="col-sm-3 works invisible">
+                    <h5 className='border-bottom mx-5 pb-1'>
+                        card 1
                     </h5>
-                    <div className="card">
+                    <div className="card mt-sm-3 bg-secondary">
                         <div className="card-body">
+                           
                         </div>
                     </div>
                 </section>
 
                 <section className="col-sm-3 works">
-                    <h5>
-                        card 5
+                <h5 className='border-bottom mx-5 pb-1'>
+                        Note Taker - Express
                     </h5>
-                    <div className="card">
+                    <div className="card mt-sm-3 bg-secondary">
                         <div className="card-body">
+                            <a href='https://github.com/ColeS82/Note_taker' target='_blank'>
+                                <img className='rounded' style={{maxWidth:'100%'}} src={noteTaker}>
+                                </img>
+                            </a>
                         </div>
                     </div>
                 </section>
 
-                <section className="col-sm-3 works">
-                    <h5>
+                <section className="col-sm-3 works invisible">
+                    <h5 className='border-bottom mx-5 pb-1'>
                         card 6
                     </h5>
-                    <div className="card">
+                    <div className="card mt-sm-3 bg-secondary">
                         <div className="card-body">
                         </div>
                     </div>
                 </section>
             </div>
 
-            <div className='d-sm-flex container justify-content-around mx-auto text-white text-center mt-sm-5 mb-5'>
+            <div className='d-sm-flex container justify-content-around mx-auto text-white text-center mt-sm-5 mb-5 invisible'>
                 <section className="col-sm-3 works">
                     <h5>
                         card 7
